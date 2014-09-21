e2237312c52821ec9b2817e442f0d2d2c03c5508
Okal Otieno <okal@justokal.com>
2014-09-21 21:28:56 +0300

    Copy over color generator from Getting Warmer

--- src/js/kupka.js
+++ src/js/kupka.js
@@ -2,6 +2,18 @@ var jsSHA = require('jssha');
 var _ = require('lodash');
 
 var Kupka = function() {
+	var generateFillColour = function(string) {
+		var color = '#', hash = 0;
+		for(var i = 0; i < string.length; i++) {
+			hash = string.charCodeAt(i) + ((hash << 5) - hash);
+		}
+		for(var i = 0; i < 3; i++) {
+			var value = (hash >> (i * 8)) & 0xFF;
+			color += ('00' + value.toString(16)).substr(-2);
+		}
+		return color;
+	};
+
 	var draw = function(IDString, width, height) {
 		var canvasWidth = width;
 		var canvasHeight = height;
